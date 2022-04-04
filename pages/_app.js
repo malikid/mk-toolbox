c168ddc6fa07b2da175ed44578400eaa48e96e96
malikid <malikid.chang@gmail.com>
2022-04-04 23:22:24 +0200

    style: add loading and have basic styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- pages/get-funda-floor-plan.js
+++ pages/get-funda-floor-plan.js
@@ -1,4 +1,14 @@
+import {useState} from 'react'
+import Head from 'next/head'
+import Stack from '@mui/material/Stack';
+import LinearProgress from '@mui/material/LinearProgress';
+import TextField from '@mui/material/TextField';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { css, cx } from '@emotion/css'
+
 export default function GetFundaFloorPlan() {
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const downloadFml = (name, fmlText) => {
     const filename = `${name.replace(' ', '_').toLowerCase()}_floorplan.fml`;
@@ -17,6 +27,7 @@ export default function GetFundaFloorPlan() {
 
   const submitUrl = async event => {
     event.preventDefault()
+    setIsLoading(true)
 
     const res = await fetch(
       '/api/funda-fml',
@@ -33,6 +44,8 @@ export default function GetFundaFloorPlan() {
 
     const result = await res.json()
 
+    setIsLoading(false)
+
     if(res.status !== 200) {
       alert(result.error)
       return
@@ -43,18 +56,56 @@ export default function GetFundaFloorPlan() {
   }
 
   return (
-    <>
-      <div>
-        <span>Warning: There is no floor plan available for sold properties!</span>
-        <form onSubmit={submitUrl}>
-          <label htmlFor="url">Enter the available / under offer property url</label>
-          <input id="url" type="url" required />
-          <button type="submit">Get</button>
-        </form>
-      </div>
-      <div>
+    <div className={css`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 20vh;
+    `}>
+      <Head>
+        <title>Download Funda Floor Plan</title>
+        <meta name="description" content="Get floor plans from funda" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <h3>Warning: There is no floor plan available for sold properties!</h3>
+      <h1>Enter the available / under offer property url</h1>
+      <form onSubmit={submitUrl} className={css`
+        display: flex;
+        flex-direction: row;
+        gap: 20px;
+      `}>
+        <TextField
+          id="url"
+          type="url"
+          variant="standard"
+          size="large"
+          required
+          className={css`
+            width: 250px;
+          `}
+        />
+        <LoadingButton
+          loading={isLoading}
+          loadingIndicator="Reaching..."
+          variant="outlined"
+          type="submit"
+          size="medium"
+        >
+          Reach!
+        </LoadingButton>
+      </form>
+      <h3>
         <a href="https:\/\/floorplanner.com">Go to FloorPlanner</a> and import the floor plan!
-      </div>
-    </>
+      </h3>
+      {isLoading &&
+        <Stack sx={{ width: '80%', color: 'grey.500' }} spacing={2}>
+          <LinearProgress color="secondary" />
+          <LinearProgress color="success" />
+          <LinearProgress color="inherit" />
+        </Stack>
+      }
+    </div>
   )
 }
