a60a92fd6c5072487807ae97cf3258b7f649039a
malikid <malikid.chang@gmail.com>
2022-04-04 23:28:39 +0200

    fix: lint

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to MK's Toolbox!
+          Welcome to MK&apos;s Toolbox!
         </h1>
 
         <p className={styles.description}>
