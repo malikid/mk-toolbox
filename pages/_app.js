8b5db224e51691c54e37e484b9ef0fc15b8946f9
malikid <malikid.chang@gmail.com>
2022-04-06 21:30:05 +0200

    chore: add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   const darkTheme = createTheme({
@@ -10,6 +11,21 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ThemeProvider theme={darkTheme}>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id='ga-analytics'>
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `
+        }
+      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
   )
